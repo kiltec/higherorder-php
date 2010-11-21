1c0b04951fa7f8c42c226514091d1dd6da82436e
Lars Tesmer <lars.tesmer@innogames.de>
2010-11-21 17:22:18 +0100

    Turned inject() into closure in order to render implementation of next example (with_list) possible.

--- 2-inject.php
+++ 2-inject.php
@@ -3,19 +3,19 @@ require('pp.php');
 require('inject.php');
 
 $sum = function($xs) {
-	return inject(0, function($a, $b) { return $a + $b;}, $xs);
+	return $inject(0, function($a, $b) { return $a + $b;}, $xs);
 };
 
 pp('sum', $sum(array(2,3)));
 
 $prod = function($xs) {
-	return inject(1, function($a, $b) { return $a * $b;}, $xs);
+	return $inject(1, function($a, $b) { return $a * $b;}, $xs);
 };
 
 pp('prod', $prod(array(2,3,4)));
 
 $grep = function($pred, $xs) {
-	return inject(array(), function($rs, $x) use($pred) {
+	return $inject(array(), function($rs, $x) use($pred) {
 		if($pred($x)) {
 		 	$rs[] = $x;
 		}
@@ -28,7 +28,7 @@ $uneven = function($x) { return $x % 2; };
 pp('grep', $grep($uneven, array(2, 4, 3, 5, 2, 126, 123)));
 
 $map = function($f, $xs) {
-	return inject(array(), function($rs, $x) use($f) {
+	return $inject(array(), function($rs, $x) use($f) {
 		$rs[] = $f($x);
 		return $rs;
  	}, $xs);
@@ -38,7 +38,7 @@ $double = function($x) { return $x * 2; };
 pp('map', $map($double, array(1, 124, 6)));
 
 $each = function($f, $xs) {
-	return inject(null, function($_, $x) use($f) {
+	return $inject(null, function($_, $x) use($f) {
 		$f($x);
 		return $_;
 	}
--- inject.php
+++ inject.php
@@ -1,9 +1,9 @@
 <?php
 
-function inject($i, $f, $xs) {
+$inject = function($i, $f, $xs) {
 	$r = $i;
 	foreach($xs as $x) {
 		$r = $f($r, $x);
 	}
 	return $r;
-}
+};
