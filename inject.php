a257e85b87c258a95fe3e7f4f3c681703fd07d7e
Lars Tesmer <lars.tesmer@innogames.de>
2010-11-21 15:53:22 +0100

    Moving example output close to function defs.

--- inject.php
+++ inject.php
@@ -13,9 +13,13 @@ $sum = function($xs) {
 	return inject(0, function($a, $b) { return $a + $b;}, $xs);
 };
 
+pp('sum', $sum(array(2,3)));
+
 $prod = function($xs) {
 	return inject(1, function($a, $b) { return $a * $b;}, $xs);
 };
+
+pp('prod', $prod(array(2,3,4)));
 
 $grep = function($pred, $xs) {
 	return inject(array(), function($rs, $x) use($pred) {
@@ -27,12 +31,18 @@ $grep = function($pred, $xs) {
 	, $xs);
 };
 
+$uneven = function($x) { return $x % 2; };
+pp('grep', $grep($uneven, array(2, 4, 3, 5, 2, 126, 123)));
+
 $map = function($f, $xs) {
 	return inject(array(), function($rs, $x) use($f) {
 		$rs[] = $f($x);
 		return $rs;
  	}, $xs);
 };
+
+$double = function($x) { return $x * 2; };
+pp('map', $map($double, array(1, 124, 6)));
 
 $each = function($f, $xs) {
 	return inject(null, function($_, $x) use($f) {
@@ -41,16 +51,6 @@ $each = function($f, $xs) {
 	}
 	, $xs);
 };
-
-pp('sum', $sum(array(2,3)));
-
-pp('prod', $prod(array(2,3,4)));
-
-$uneven = function($x) { return $x % 2; };
-pp('grep', $grep($uneven, array(2, 4, 3, 5, 2, 126, 123)));
-
-$double = function($x) { return $x * 2; };
-pp('map', $map($double, array(1, 124, 6)));
 
 $double_echo = function($x) { echo $x * 2; };
 pp('each', $each($double_echo, array(1, 124, 6)));
