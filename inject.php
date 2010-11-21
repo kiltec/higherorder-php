e9f28e996b767122f8d63bb1e659175ce0a2ab82
Lars Tesmer <lars.tesmer@innogames.de>
2010-11-21 14:32:52 +0100

    Searching through an array implemented using inject
    At first, forgot to tell PHP to use $pred.

--- inject.php
+++ inject.php
@@ -17,5 +17,17 @@ $prod = function($xs) {
 	return inject(1, function($a, $b) { return $a * $b;}, $xs);
 };
 
+$grep = function($pred, $xs) {
+	return inject(array(), function($rs, $x) use($pred){
+		if($pred === $x) {
+		 	$rs[] = $x;
+		}
+		return $rs;
+	}
+	, $xs);
+};
+
+
 pp('sum', $sum(array(2,3)));
 pp('prod', $prod(array(2,3,4)));
+pp('grep', implode(',', $grep('lars', array('bernd', 'karl', 'lars', 'tim', 'lars'))));
